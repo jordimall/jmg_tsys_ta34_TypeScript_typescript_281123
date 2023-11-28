dfdf236eb00c0ffbed2710cb06fc02b5ad5412e4
jordimall <jordi.mallafre@hotmail.com>
2023-11-28 23:40:27 +0100

    Finalización ejercicio 1

---
+++ build/src/Ejercicio1/enumSexo.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SEXO_DEFECTO = exports.enumSexo = void 0;
+var enumSexo;
+(function (enumSexo) {
+    enumSexo["H"] = "HOMBRE";
+    enumSexo["M"] = "MUJER";
+})(enumSexo || (exports.enumSexo = enumSexo = {}));
+exports.SEXO_DEFECTO = enumSexo.H;
---
+++ build/src/main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ejercicio1_1 = require("./Ejercicio1/ejercicio1");
+(0, ejercicio1_1.ejercicio1)();
